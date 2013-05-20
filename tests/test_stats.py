a2b2d76b47ff7379ddd2a40ddae4b7a0a2d8059e
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-20 15:22:32 -0400

    Update unit tests

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -10,13 +10,32 @@ np.seterr(all='raise')
 
 from stats import normalize
 
+N_big = 20
+N_small = 5
+thresh = 1e-6
+
+
+def rand_h():
+    h = np.random.uniform(0, 2)
+    return h
+
+
+def rand_w():
+    w = np.random.uniform(np.pi / 32., np.pi / 2.)
+    return w
+
+
+def rand_p():
+    p = np.random.uniform(0.33, 3)
+    return p
+
 
 def check_normalization_constants(arr, axis):
     sum = np.log(np.sum(arr, axis=axis))
     z = normalize(np.log(arr), axis=axis)[0]
 
     zdiff = np.abs(sum - z)
-    if not (zdiff < 1e-6).all():
+    if not (zdiff < thresh).all():
         print sum
         print z
         raise AssertionError("wrong normalization constant")
@@ -28,7 +47,7 @@ def check_normalization(arr, axis):
     n = normalize(np.log(arr), axis=axis)[1]
 
     ndiff = np.abs(norm - n)
-    if not(ndiff < 1e-6).all():
+    if not(ndiff < thresh).all():
         print norm
         print n
         raise AssertionError("wrong normalized values")
@@ -36,7 +55,7 @@ def check_normalization(arr, axis):
 
 def test_normalize_10():
     """Test stats.normalize for a vector"""
-    for i in xrange(5):
+    for i in xrange(N_big):
         arr = np.random.gamma(2, scale=2, size=10)
         yield (check_normalization_constants, arr, 0)
         yield (check_normalization, arr, 0)
@@ -44,7 +63,7 @@ def test_normalize_10():
 
 def test_normalize_5x10x15():
     """Test stats.normalize for a multidimensional array"""
-    for i in xrange(5):
+    for i in xrange(N_big):
         arr = np.random.gamma(2, scale=2, size=(5, 15, 20))
         for axis in xrange(3):
             yield (check_normalization_constants, arr, axis)
@@ -53,7 +72,7 @@ def test_normalize_5x10x15():
 
 def test_normalize_2x100000():
     """Test stats.normalize for a large array"""
-    for i in xrange(1):
+    for i in xrange(N_small):
         arr = np.random.gamma(2, scale=2, size=(2, 100000))
         for axis in xrange(2):
             yield (check_normalization_constants, arr, axis)
@@ -68,30 +87,29 @@ from stats import GP, gaussian_kernel, periodic_kernel
 
 
 def check_gaussian_kernel(x, dx, h, w):
-    pdx = scipy.stats.norm.pdf(dx, loc=0, scale=w)
-    pdx *= (h ** 2) * np.sqrt(2 * np.pi) * w
     kernel = gaussian_kernel(h, w, jit=False)
     K = kernel(x, x)
+    print "Kernel parameters:", kernel.params
+
+    pdx = scipy.stats.norm.pdf(dx, loc=0, scale=w)
+    pdx *= (h ** 2) * np.sqrt(2 * np.pi) * w
 
     diff = abs(pdx - K)
-    if not (diff < 1e-6).all():
-        print pdx
-        print K
+    if not (diff < thresh).all():
+        print diff
         raise AssertionError("invalid kernel matrix")
 
 
-def check_periodic_kernel(x, dx, h, w):
-    cos_pdx = scipy.stats.norm.pdf(dx[0], loc=0, scale=w)
-    sin_pdx = scipy.stats.norm.pdf(dx[1], loc=0, scale=w)
-    pdx = cos_pdx * sin_pdx
-    pdx *= (h ** 2) * (np.sqrt(2 * np.pi) * w) ** 2
-    kernel = periodic_kernel(h, w, jit=False)
+def check_periodic_kernel(x, dx, h, w, p):
+    kernel = periodic_kernel(h, w, p, jit=False)
     K = kernel(x, x)
+    print "Kernel parameters:", kernel.params
+
+    pdx = (h ** 2) * np.exp(-2. * (np.sin(dx / (2. * p)) ** 2) / (w ** 2))
 
     diff = abs(pdx - K)
-    if not (diff < 1e-6).all():
-        print pdx
-        print K
+    if not (diff < thresh).all():
+        print diff
         raise AssertionError("invalid kernel matrix")
 
 
@@ -115,7 +133,7 @@ def test_gaussian_kernel():
     """Test stats.gaussian_kernel output matrix"""
     x = np.linspace(-2, 2, 10)
     dx = x[:, None] - x[None, :]
-    for i in xrange(10):
+    for i in xrange(N_big):
         h, w = np.random.gamma(2, scale=2, size=2)
         yield (check_gaussian_kernel, x, dx, h, w)
 
@@ -123,36 +141,36 @@ def test_gaussian_kernel():
 @raises(ValueError)
 def test_periodic_kernel_params1():
     """Test invalid h parameter to stats.periodic_kernel"""
-    periodic_kernel(0, 1, False)
+    periodic_kernel(0, 1, 1, False)
 
 
 @raises(ValueError)
 def test_periodic_kernel_params2():
     """Test invalid w parameter to stats.periodic_kernel"""
-    periodic_kernel(1, 0, False)
+    periodic_kernel(1, 0, 1, False)
 
 
 # def test_periodic_kernel_jit():
-#     periodic_kernel(1, 1, True)
+#     periodic_kernel(1, 1, 1, True)
 
 
 def test_periodic_kernel():
     """Test stats.periodic_kernel output matrix"""
     x = np.linspace(-2*np.pi, 2*np.pi, 16)
-    u = np.array([np.cos(x), np.sin(x)])
-    dx = u[:, :, None] - u[:, None, :]
-    for i in xrange(10):
-        h, w = np.random.gamma(2, scale=2, size=2)
-        yield (check_periodic_kernel, x, dx, h, w)
+    dx = x[:, None] - x[None, :]
+    for i in xrange(N_big):
+        h = rand_h()
+        w = rand_w()
+        p = rand_p()
+        yield (check_periodic_kernel, x, dx, h, w, p)
 
 
 def check_GP(kernel, x, y, s):
-    print kernel.h, kernel.w
+    print "Kernel parameters:", kernel.params
     mean, cov = GP(kernel, x, y, x, s=s)
     diff = abs(y - mean)
-    if not (diff < 1e-6).all():
-        print y
-        print mean
+    if not (diff < thresh).all():
+        print "Error:"
         print diff
         raise AssertionError("incorrect GP predictions")
 
@@ -160,13 +178,9 @@ def check_GP(kernel, x, y, s):
 def test_gaussian_GP():
     x = np.linspace(-2*np.pi, 2*np.pi, 16)
     y = np.sin(x)
-    for i in xrange(10):
-        h = np.random.gamma(2, scale=2)
-        w = np.random.gamma(1, scale=2)
-        # don't let w get too big, or we'll get a singular matrix
-        # (why??)
-        if w > np.pi:
-            w = np.pi
+    for i in xrange(N_big):
+        h = rand_h()
+        w = rand_w()
         kernel = gaussian_kernel(h, w, jit=False)
         yield (check_GP, kernel, x, y, 0)
 
@@ -176,12 +190,9 @@ def test_periodic_GP():
     # resulting covariance matrix will be singular!
     x = np.linspace(-2*np.pi, 2*np.pi-np.radians(1), 16)
     y = np.sin(x)
-    for i in xrange(10):
-        h = np.random.gamma(2, scale=2)
-        w = np.random.gamma(1, scale=2)
-        # don't let w get too big, or we'll get a singular matrix
-        # (why??)
-        if w > np.pi / 2.:
-            w = np.pi / 2.
-        kernel = periodic_kernel(h, w, jit=False)
+    for i in xrange(N_big):
+        h = rand_h()
+        w = rand_w()
+        p = rand_p()
+        kernel = periodic_kernel(h, w, p, jit=False)
         yield (check_GP, kernel, x, y, 0)
