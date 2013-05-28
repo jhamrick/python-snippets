530686616d521fb3bbe83f52e66ab430cf2202ac
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-27 22:15:22 -0400

    Add "safe math" functions for handling overflow/underflow

--- stats.py
+++ stats.py
@@ -28,64 +28,11 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import numpy as np
 import numba
-import sys
 
 from numpy import log, exp, dot
 from numpy.linalg import inv
 
-# constants for largest/smallest log values we can handle/
-MIN_LOG = np.log(sys.float_info.min)
-MAX_LOG = np.log(sys.float_info.max)
-
-
-def normalize(logarr, axis=-1):
-    """Normalize an array of log-values.
-
-    This function is very useful if you have an array of log
-    probabilities that need to be normalized, but some of the
-    probabilies might be extremely small (i.e., underflow will occur if
-    you try to exponentiate them). This function computes the
-    normalization constants in log space, thus avoiding the need to
-    exponentiate the values.
-
-    Parameters
-    ----------
-    logarr: numpy.ndarray
-        Array of log values
-    axis: integer (default=-1)
-        Axis over which to normalize
-
-    Returns
-    -------
-    out: (numpy.ndarray, numpy.ndarray)
-        2-tuple consisting of the log normalization constants used to
-        normalize the array, and the normalized array of log values
-
-    """
-
-    # shape for the normalization constants (that would otherwise be
-    # missing axis)
-    shape = list(logarr.shape)
-    shape[axis] = 1
-    # get maximum value of array
-    maxlogarr = logarr.max(axis=axis).reshape(shape)
-    # calculate how much to shift the array up by
-    shift = MAX_LOG - maxlogarr - 2 - logarr.shape[axis]
-    shift[shift < 0] = 0
-    # shift the array
-    unnormed = logarr + shift
-    # convert from logspace
-    arr = exp(unnormed)
-    # calculate shifted log normalization constants
-    _lognormconsts = log(arr.sum(axis=axis)).reshape(shape)
-    # calculate normalized array
-    lognormarr = unnormed - _lognormconsts
-    # unshift normalization constants
-    _lognormconsts -= shift
-    # get rid of the dimension we normalized over
-    lognormconsts = _lognormconsts.sum(axis=axis)
-
-    return lognormconsts, lognormarr
+from safemath import MIN_LOG, MAX_LOG, normalize
 
 
 def GP(K, x, y, xo, s=0):
---
+++ tests/test_safemath.py
@@ -0,0 +1,94 @@
+import numpy as np
+np.seterr(all='raise')
+
+N_big = 20
+N_small = 5
+thresh = 1e-6
+
+######################################################################
+# normalize
+######################################################################
+
+from safemath import normalize
+
+
+def check_normalization_constants(arr, axis):
+    sum = np.log(np.sum(arr, axis=axis))
+    z = normalize(np.log(arr), axis=axis)[0]
+
+    zdiff = np.abs(sum - z)
+    if not (zdiff < thresh).all():
+        print sum
+        print z
+        raise AssertionError("wrong normalization constant")
+
+
+def check_normalization(arr, axis):
+    sum = np.sum(arr, axis=axis)
+    norm = np.log(arr / np.expand_dims(sum, axis=axis))
+    n = normalize(np.log(arr), axis=axis)[1]
+
+    ndiff = np.abs(norm - n)
+    if not(ndiff < thresh).all():
+        print norm
+        print n
+        raise AssertionError("wrong normalized values")
+
+
+def test_normalize_10():
+    """Test stats.normalize for a vector"""
+    for i in xrange(N_big):
+        arr = np.random.gamma(2, scale=2, size=10)
+        yield (check_normalization_constants, arr, 0)
+        yield (check_normalization, arr, 0)
+
+
+def test_normalize_5x10x15():
+    """Test stats.normalize for a multidimensional array"""
+    for i in xrange(N_big):
+        arr = np.random.gamma(2, scale=2, size=(5, 15, 20))
+        for axis in xrange(3):
+            yield (check_normalization_constants, arr, axis)
+            yield (check_normalization, arr, axis)
+
+
+def test_normalize_2x100000():
+    """Test stats.normalize for a large array"""
+    for i in xrange(N_small):
+        arr = np.random.gamma(2, scale=2, size=(2, 100000))
+        for axis in xrange(2):
+            yield (check_normalization_constants, arr, axis)
+            yield (check_normalization, arr, axis)
+
+
+######################################################################
+# safe_log
+######################################################################
+
+from safemath import safe_log
+
+
+def test_safe_log():
+    arr = np.random.rand(10, 10) + 1
+    log_arr1 = np.log(arr)
+    log_arr2 = safe_log(arr)
+    assert (log_arr1 == log_arr2).all()
+
+
+######################################################################
+# safe_multiply
+######################################################################
+
+from safemath import safe_multiply
+
+
+def test_safe_multiply():
+    arr1 = np.random.randn(10, 10)
+    arr2 = np.random.randn(10, 10)
+    prod1 = arr1 * arr2
+    prod2 = safe_multiply(arr1, arr2)
+    diff = np.abs(prod1 - prod2)
+    if not (diff < 1e-8).all():
+        print prod1
+        print prod2
+        raise ValueError
