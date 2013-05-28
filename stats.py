cb5dfec3d0faea334c6770093388ae70c7f627e6
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-28 11:18:57 -0400

    Add xcorr to stats.py

--- stats.py
+++ stats.py
@@ -32,6 +32,7 @@ import numba
 from numpy import log, exp, dot
 from numpy.linalg import inv
 
+import circstats as circ
 from safemath import MIN_LOG, MAX_LOG, normalize
 
 
@@ -263,3 +264,79 @@ def periodic_kernel(h, w, p, jit=True):
         K = kernel
 
     return K
+
+
+def xcorr(x, y, circular=False, deg=False, nanrobust=False):
+    """Returns matrix of correlations between x and y
+
+    Parameters
+    ----------
+    x : np.ndarray
+        Columns are different parameters, rows are sets of observations
+    y : np.ndarray
+        Columns are different parameters, rows are sets of observations
+    circular : bool (default=False)
+        Whether or not the data is circular
+    deg : bool (default=False)
+        Whether or not the data is in degrees (if circular)
+
+    Returns
+    -------
+    out : np.ndarray
+        Matrix of correlations between rows of x and y
+    """
+
+    # Inputs' shapes
+    xshape = x.shape
+    yshape = y.shape
+
+    # Store original (output) shapes
+    corrshape = xshape[:-1] + yshape[:-1]
+
+    # Prepares inputs' shapes for computations
+    if len(x.shape) > 2:
+        x = x.reshape((np.prod(xshape[:-1]), xshape[-1]), order='C')
+    if len(y.shape) > 2:
+        y = y.reshape((np.prod(yshape[:-1]), yshape[-1]), order='C')
+
+    if x.ndim == 1:
+        x = x[None, :]
+    if y.ndim == 1:
+        y = y[None, :]
+
+    if circular:
+        if deg:
+            x = np.radians(x)
+            y = np.radians(y)
+
+        # if nanrobust:
+        #     corr = circ.nancorrcc(x, y, axis=1)
+        # else:
+        #     corr = circ.corrcc(x, y, axis=1, nancheck=False)
+        if nanrobust:
+            corr = circ.nancorrcc(x[:, :, None], y.T[None, :, :], axis=1)
+        else:
+            corr = circ.corrcc(x[:, :, None], y.T[None, :, :], axis=1)
+
+    else:
+        avgfn = np.mean
+        stdfn = np.std
+
+        # numerator factors (centered means)
+        nx = (x.T - avgfn(x, axis=1)).T
+        ny = (y.T - avgfn(y, axis=1)).T
+
+        # denominator factors (std devs)
+        sx = stdfn(x, axis=1)
+        sy = stdfn(y, axis=1)
+
+        # numerator
+        num = np.dot(nx, ny.T) / x.shape[1]
+
+        # correlation
+        corr = num / np.outer(sx, sy)
+
+    # reshape to take original
+    corr = corr.reshape(corrshape, order='F')
+
+    return corr
